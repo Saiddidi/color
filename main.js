cc7f501c5ad48a5ac5a154fd4fe694f0ed2153a4
Saiddidi <126246149+Saiddidi@users.noreply.github.com>
2024-05-17 12:10:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let box = document.querySelector("div");
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+setInterval(() => {
+  if (box.style.borderRadius === "") {
+    console.log("h");
+    box.style.borderRadius = "50%";
+  } else {
+    box.style.borderRadius = "";
+    console.log("n");
+  }
+  box.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+}, 3000);
